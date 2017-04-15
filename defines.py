052b48be59fb6b99dfad1a63dbfaead0d6280019
hipresario <y070049@gmail.com>
2017-04-15 22:52:40 +0800

    refactor

---
+++ defines.py
@@ -0,0 +1,15 @@
+class Data:
+    def __init__(self):
+        self.ATTR = 'DEFAULT_PAYMENT_NEXT_MONTH'
+        self.SOURCE = 'credit_cards_default.csv'
+        self.SOURCE_CLEAN_1 = 'all_clean_no_noise.csv'
+        self.SOURCE_CLEAN_2 = 'all_clean_has_noise.csv'
+        self.SOURCE_DUMMY_1 = 'all_clean_dummy_no_noise.csv'
+        self.SOURCE_DUMMY_2 = 'all_clean_dummy_has_noise.csv'
+        self.NO_NOISY = True
+
+    def getFile(self):
+        return self.SOURCE_CLEAN_1 if self.NO_NOISY else self.SOURCE_CLEAN_2
+
+    def getDummyFile(self):
+        return self.SOURCE_DUMMY_1 if self.NO_NOISY else self.SOURCE_DUMMY_2
--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,14 @@
 import pandas as pd
-import decimal
-from sklearn.utils import shuffle
+import defines
 
 total_strange_default = 0
 total_strange_ok = 0
-attr = 'DEFAULT_PAYMENT_NEXT_MONTH'
+
+D = defines.Data()
+
+def updateD(data):
+    global D
+    D = data
 
 def checkPayStatus(row):
     flag = -2
@@ -28,83 +32,92 @@ def checkTotalBill(row):
     for i in range(1, 7):
         bill += float(row['BILL_AMT' + str(i)])
 
-    if row['BILL_AMT1'] == 0 and bill == 0 and row[attr] == 1:
-        total_strange_default += 1
-        flag = -2
+    if row['BILL_AMT1'] == 0 and bill == 0:
+        if row[D.ATTR] == 1:
+            total_strange_default += 1
+            flag = -2
 
-    if row['BILL_AMT1'] == 0 and bill == 0 and row[attr] == 0:
-        total_strange_ok += 1
-        flag = -2
+        if row[D.ATTR] == 0:
+            total_strange_ok += 1
+            flag = -2
 
     return flag
 
+def fixEducation(row):
+    if row['EDUCATION'] > 4:
+        return 4
+
+    return row['EDUCATION']
+
 def cleanData():
-    df = pd.read_csv('credit_cards_default.csv')
+    df = pd.read_csv(D.SOURCE)
 
-    df['PAY_WRONG'] = df.apply(checkPayStatus, axis=1)
-    df['BILL_WRONG'] = df.apply(checkTotalBill, axis=1)
+    df['PAY_POSSIBLE_WRONG'] = df.apply(checkPayStatus, axis=1)
+    df['BILL_POSSIBLE_WRONG'] = df.apply(checkTotalBill, axis=1)
 
-    df.drop(df[df.PAY_WRONG == -2].index, inplace=True)
-    df.drop(df[df.BILL_WRONG == -2].index, inplace=True)
+    df.drop(df[df.PAY_POSSIBLE_WRONG == -2].index, inplace=True)
+    df.drop(df[df.BILL_POSSIBLE_WRONG == -2].index, inplace=True)
 
-    df = df.drop(['PAY_WRONG', 'BILL_WRONG'], axis=1)
+    df = df.drop(['PAY_POSSIBLE_WRONG', 'BILL_POSSIBLE_WRONG'], axis=1)
+
+    if D.NO_NOISY:
+        df['EDUCATION'] = df.apply(fixEducation, axis=1)
+        df = df[df['MARRIAGE'] != 0]
+        df = df[df['EDUCATION'] != 0]
 
     print(total_strange_default)
     print(total_strange_ok)
 
-    df.to_csv('all_data_clean.csv', index=False)
+    df.to_csv(D.getFile(), index=False)
 
 def readCleanData():
-    return pd.read_csv('all_data_clean.csv')
+    return pd.read_csv(D.getFile())
 
 def statistics():
     df = readCleanData()
-    print(df.describe())
+    print('Total records: ')
+    print(df[D.ATTR].count())
     print('Default sum: ')
-    print(df[attr].sum())
+    print(df[D.ATTR].sum())
 
 def createDummyVar():
     df = readCleanData()
     df = pd.get_dummies(df, prefix='GENDER_', columns=['SEX'])
     df = pd.get_dummies(df, prefix='EDUCATION_', columns=['EDUCATION'])
     df = pd.get_dummies(df, prefix='MARRIAGE_', columns=['MARRIAGE'])
-    df.to_csv('all_data_clean_with_dummy.csv', index=False)
 
+    df.to_csv(D.getDummyFile(), index=False)
 
-def convert(row):
+# def convert(row, i):
+#     j = str(i)
+#     bill = row['BILL_AMT' + j]
+#     pay = row['PAY_AMT' + j]
+#     if bill <= 0:
+#         return 0
+#     else:
+#         if pay == 0:
+#             return bill
+#         else:
+#             return bill / pay
+#     return 0
 
-    for i in range(1, 7):
-        j = str(i)
-        bill = row['BILL_AMT' + j]
-        pay = row['PAY_AMT' + j]
-        if bill <= 0:
-            row['BILL_TO_PAY' + j] = 0
-        else:
-            if pay == 0:
-                row['BILL_TO_PAY' + j] = 1
-            else:
-                row['BILL_TO_PAY' + j] = bill / pay
-
-
-def featureEngineering():
-    df = readDataWithDummy()
-    df = df.apply(convert, axis=1)
-    # for index, row in df.iterrows():
-    #     row = convert(row)
-
-    df.to_csv('all_data_clean_with_dummy_extra.csv', index=False)
-
-def readDataWithDummy():
-    return pd.read_csv('all_data_clean_with_dummy.csv')
+#no improvement
+# def featureEngineering():
+#     df = readDataWithDummy()
+#     df['BILL_TO_PAY1'] = df.apply(lambda row: convert(row, 1), axis=1)
+#     df['BILL_TO_PAY2'] = df.apply(lambda row: convert(row, 2), axis=1)
+#     df['BILL_TO_PAY3'] = df.apply(lambda row: convert(row, 3), axis=1)
+#     df['BILL_TO_PAY4'] = df.apply(lambda row: convert(row, 4), axis=1)
+#     df['BILL_TO_PAY5'] = df.apply(lambda row: convert(row, 5), axis=1)
+#     df['BILL_TO_PAY6'] = df.apply(lambda row: convert(row, 6), axis=1)
+#
+#     df.to_csv('all_data_clean_with_dummy_extra.csv', index=False)
 
 def main():
-    #cleanData()
-    #statistics()
-    #createDummyVar()
-    featureEngineering()
-
-
-
+    cleanData()
+    statistics()
+    createDummyVar()
+    #featureEngineering()
 
 if __name__ == '__main__':
     main()
