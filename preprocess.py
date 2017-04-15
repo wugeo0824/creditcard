9bcde9c52c844d22ce6b4d695294e9831b9061fa
hipresario <y070049@gmail.com>
2017-04-15 17:25:48 +0800

    update

--- preprocess.py
+++ preprocess.py
@@ -2,63 +2,108 @@ import pandas as pd
 import decimal
 from sklearn.utils import shuffle
 
+total_strange_default = 0
+total_strange_ok = 0
+attr = 'DEFAULT_PAYMENT_NEXT_MONTH'
+
+def checkPayStatus(row):
+    flag = -2
+
+    if row['PAY_0'] == -2:
+        return flag
+
+    for i in range(2, 7):
+        if row['PAY_' + str(i)] == -2:
+            return flag
+
+    return 0
+
+def checkTotalBill(row):
+    global total_strange_default
+    global total_strange_ok
+
+    flag = 0
+    bill = 0
+
+    for i in range(1, 7):
+        bill += float(row['BILL_AMT' + str(i)])
+
+    if row['BILL_AMT1'] == 0 and bill == 0 and row[attr] == 1:
+        total_strange_default += 1
+        flag = -2
+
+    if row['BILL_AMT1'] == 0 and bill == 0 and row[attr] == 0:
+        total_strange_ok += 1
+        flag = -2
+
+    return flag
+
+def cleanData():
+    df = pd.read_csv('credit_cards_default.csv')
+
+    df['PAY_WRONG'] = df.apply(checkPayStatus, axis=1)
+    df['BILL_WRONG'] = df.apply(checkTotalBill, axis=1)
+
+    df.drop(df[df.PAY_WRONG == -2].index, inplace=True)
+    df.drop(df[df.BILL_WRONG == -2].index, inplace=True)
+
+    df = df.drop(['PAY_WRONG', 'BILL_WRONG'], axis=1)
+
+    print(total_strange_default)
+    print(total_strange_ok)
+
+    df.to_csv('all_data_clean.csv', index=False)
+
+def readCleanData():
+    return pd.read_csv('all_data_clean.csv')
+
+def statistics():
+    df = readCleanData()
+    print(df.describe())
+    print('Default sum: ')
+    print(df[attr].sum())
+
+def createDummyVar():
+    df = readCleanData()
+    df = pd.get_dummies(df, prefix='GENDER_', columns=['SEX'])
+    df = pd.get_dummies(df, prefix='EDUCATION_', columns=['EDUCATION'])
+    df = pd.get_dummies(df, prefix='MARRIAGE_', columns=['MARRIAGE'])
+    df.to_csv('all_data_clean_with_dummy.csv', index=False)
+
+
+def convert(row):
+
+    for i in range(1, 7):
+        j = str(i)
+        bill = row['BILL_AMT' + j]
+        pay = row['PAY_AMT' + j]
+        if bill <= 0:
+            row['BILL_TO_PAY' + j] = 0
+        else:
+            if pay == 0:
+                row['BILL_TO_PAY' + j] = 1
+            else:
+                row['BILL_TO_PAY' + j] = bill / pay
+
+
+def featureEngineering():
+    df = readDataWithDummy()
+    df = df.apply(convert, axis=1)
+    # for index, row in df.iterrows():
+    #     row = convert(row)
+
+    df.to_csv('all_data_clean_with_dummy_extra.csv', index=False)
+
+def readDataWithDummy():
+    return pd.read_csv('all_data_clean_with_dummy.csv')
+
 def main():
-    train = pd.DataFrame(pd.read_csv('train_class.csv'))
-    train['Y'] = train['Y'].map({'Yes': 1, 'No': 0})
-    train.to_csv('train.csv', index=False)
+    #cleanData()
+    #statistics()
+    #createDummyVar()
+    featureEngineering()
 
-    validation = pd.DataFrame(pd.read_csv('validation_class.csv'))
-    validation['Y'] = validation['Y'].map({'Yes': 1, 'No': 0})
-    validation.to_csv('validation.csv', index=False)
 
-    test = pd.DataFrame(pd.read_csv('test_class.csv'))
-    test['Y'] = test['Y'].map({'Yes': 1, 'No': 0})
-    test.to_csv('test.csv', index=False)
-#
-# def main():
-#     all = pd.DataFrame(pd.read_csv('alldata.csv'))
-#     all['Y'] = all['Y'].map({1: 'Yes', 0: 'No'})
-#     total = all.shape[0]
-#     #all.to_csv('alldata_class.csv', index=False)
-#
-#     full = pd.DataFrame(pd.read_csv('alldata_class.csv'))
-#
-#     default_data = full.loc[full['Y'] == 'Yes']
-#     nondefault_data = full.loc[full['Y'] == 'No']
-#
-#     default_pct = round((default_data['Y'].value_counts()) / total, 2)
-#     nondefault_pct = 1 - default_pct
-#
-#     print(default_pct)
-#     print(nondefault_pct)
-#
-#     #40% training, 10% validation, 50% test
-#     train_default_count =  int(0.4 * default_pct * total)
-#     train_nondefault_count = int(0.4 * nondefault_pct * total)
-#
-#     validation_default_count = int (0.1 * default_pct * total)
-#     validation_nondefault_count = int(0.1  * nondefault_pct * total)
-#
-#     train_default_df = default_data.head(train_default_count)
-#     train_nondefault_df = nondefault_data.head(train_nondefault_count)
-#     train = pd.concat([train_default_df, train_nondefault_df])
-#     #print(train)
-#     train = shuffle(train)
-#     train.to_csv('train_class.csv', index=False)
-#
-#     validation_default_df = default_data[train_default_count: train_default_count + validation_default_count]
-#     validation_nondefault_df = nondefault_data[train_nondefault_count: train_nondefault_count + validation_nondefault_count]
-#     validation = pd.concat([validation_default_df, validation_nondefault_df])
-#     # print(validation)
-#     validation = shuffle(validation)
-#     validation.to_csv('validation_class.csv', index=False)
-#
-#     test_default_df = default_data[train_default_count + validation_default_count:]
-#     test_nondefault_df = nondefault_data[train_nondefault_count + validation_nondefault_count:]
-#     test = pd.concat([test_default_df, test_nondefault_df])
-#     # print(test)
-#     test = shuffle(test)
-#     test.to_csv('test_class.csv', index=False)
 
 
 if __name__ == '__main__':
